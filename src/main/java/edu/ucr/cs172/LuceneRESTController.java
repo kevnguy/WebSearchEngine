e6c2969c8214d9ead45fefdc6328bbc8bf355d20
kevnguy <56405177+kevnguy@users.noreply.github.com>
2022-05-22 00:28:49 -0700

    Update index properties to add range queries Update REST controller to reflect search queries on frontend

--- src/main/java/edu/ucr/cs172/LuceneRESTController.java
+++ src/main/java/edu/ucr/cs172/LuceneRESTController.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.ArrayList;
 
 @SpringBootApplication(/*exclude = MongoAutoConfiguration.class*/)
@@ -18,20 +21,21 @@ public class LuceneRESTController {
 	}
 
 	@GetMapping(value = "/api/search")
-	public ResponseEntity<ArrayList> hello2(
-			@RequestParam String q,
-			@RequestParam(required = false) String title,
-			@RequestParam(required = false) String or)throws Exception {
-		String queryTitle = title;
-		String boolQuery = or;
-		String queryMain = q;
-		ArrayList response = SearchDocs.searchDocuments(q);
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
-
-	@GetMapping("/hello")
-	public String hello(){
-		return "Hello World fk lols top led" +
-				"holysht it workd";
+	@CrossOrigin(origins = "http://localhost:3000")
+	public ResponseEntity<ArrayList> hello2(@RequestParam String q)throws Exception {
+		if(!q.isEmpty()) {
+			try{
+				System.out.println(q);
+				ArrayList response = SearchDocs.searchDocuments(q);
+				return new ResponseEntity<>(response, HttpStatus.OK);
+			}
+			catch (Exception e){
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+			}
+		}
+		else
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error: Query not provided or unable to parse query");
 	}
 }
+
+
